e1f069dd959037dde6de69148cc3c5ef6cbdd0f1
Michael Borromeo <mbborromeo@gmail.com>
2019-08-16 01:48:57 -0700

    added MD5 Hash

--- src/app/marvel-api-call.service.ts
+++ src/app/marvel-api-call.service.ts
@@ -3,7 +3,8 @@ import { Observable, of } from 'rxjs';
 import { Character } from './character/character';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders  } from '@angular/common/http';
-import {catchError, map, tap} from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
+import md5 from 'crypto-js/md5';
 
 interface MarvelResponse {
   readonly items: Character[];
@@ -19,17 +20,15 @@ export class MarvelApiCallService {
     private messageService: MessageService
   ) { }
 
+  //Auth signing: https://github.com/brix/crypto-js
+  //https://stackoverflow.com/questions/38626334/rest-api-call-through-marvel-dont-know-syntax-works-with-md5
   private publicKey = '071a199f0748e4784ec9d1a918a6c22a';
   private privateKey = '5a074608a129b10cb01e080985b97624e4b46187';
   private ts = new Date().getTime();
   private stringToHash = this.ts + this.privateKey + this.publicKey;
-  private hash = /*md5(*/ this.stringToHash /*)*/;
-  //private callbackParamKey = 'jsoncallback'; //Angular will assign its own function ng_jsonp_callback_0 as the callback argument for API call in URL
-
-  //private marvelUrl = 'https://gateway.marvel.com:443/v1/public/characters?apikey=' + this.publicKey;  
+  private hash = md5( this.stringToHash ).toString();
   private marvelUrl = 'https://gateway.marvel.com:443/v1/public/characters?ts=' + this.ts + "&apikey=" + this.publicKey + "&hash=" + this.hash;
-  
-  
+    
   /** Log a PhotoService message with the MessageService */
   private log(message: string) {
     this.messageService.add(`MarvelApiCallService: ${message}`);
